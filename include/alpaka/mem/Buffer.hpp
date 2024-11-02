6e53188556d7b6b3a59ca44e29eac89f625732a1
René Widera <r.widera@hzdr.de>
2024-11-02 19:02:21 +0100

    mdSpan support
    - implement own MdSpan over original mdSpan to support `operator[]`
    - switch example to MdSpan

--- include/alpaka/mem/Buffer.hpp
+++ include/alpaka/mem/Buffer.hpp
@@ -1,13 +1,13 @@
-/* Copyright 2024 René Widera
+/* Copyright 2024 Bernhard Manfred Gruber, René Widera
  * SPDX-License-Identifier: MPL-2.0
  */
-
 
 #pragma once
 
 #include "alpaka/core/Handle.hpp"
 #include "alpaka/core/config.hpp"
 #include "alpaka/hostApi.hpp"
+#include "alpaka/mem/MdSpan.hpp"
 
 #include <cstdint>
 #include <functional>
@@ -41,6 +41,28 @@ namespace alpaka
         auto getExtent() const
         {
             return m_extents;
+        }
+
+        auto getMdSpan() const
+        {
+            // import mdspan into alpaka::experimental namespace. see: https://eel.is/c++draft/mdspan.syn
+            using std::experimental::default_accessor;
+            using std::experimental::dextents;
+            using std::experimental::extents;
+            using std::experimental::layout_left;
+            using std::experimental::layout_right;
+            using std::experimental::layout_stride;
+            using std::experimental::mdspan;
+            // import submdspan as well, which is not standardized yet
+            using std::experimental::full_extent;
+            using std::experimental::submdspan;
+
+            auto* ptr = reinterpret_cast<std::byte*>(data(m_data));
+            auto ex = detail::makeExtents(m_extents, std::make_index_sequence<T_Extents::dim()>{});
+            auto const strides = m_data->m_pitch;
+            layout_stride::mapping<decltype(ex)> m{ex, strides.toStdArray()};
+            return alpaka::MdSpan{
+                mdspan<type, decltype(ex), layout_stride, detail::ByteIndexedAccessor<type>>{ptr, m}};
         }
 
     private:
