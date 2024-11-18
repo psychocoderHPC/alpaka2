cb21fcddd4f26b4617440147b956cfb152661005
René Widera <r.widera@hzdr.de>
2024-11-18 10:28:44 +0100

    add IndexRange to seperate index operations

--- example/heatEquation2D/src/StencilKernel.hpp
+++ example/heatEquation2D/src/StencilKernel.hpp
@@ -73,7 +73,8 @@ struct StencilKernel
         double const rY = dt / (dy * dy);
 
         // go over only core cells
-        for(auto idx2D : alpaka::makeIter(acc, alpaka::iter::withinDataFrame) >> 1u)
+        for(auto idx2D :
+            alpaka::makeIter(acc, alpaka::iter::withinDataFrame, alpaka::Vec{1u, 1u}, alpaka::Vec{16u, 16u}))
         {
             //  idx2D = idx2D + IdxVec{1, 1}; // offset for halo above and to the left
             auto localIdx1D = alpaka::linearize(chunkSize + halo, idx2D);
