f5f37c6b53b2e1f14f4c24084ab9a61852a328a7
René Widera <r.widera@hzdr.de>
2024-11-22 20:42:28 +0100

    refactor iterator creation

--- tests/block.cpp
+++ tests/block.cpp
@@ -24,11 +24,11 @@ struct BlockIotaKernel
 {
     ALPAKA_FN_ACC void operator()(auto const& acc, auto out, auto numBlocks) const
     {
-        for(auto blockIdx : onAcc::makeIter(acc, onAcc::iter::overDataFrames.over(IdxRange{numBlocks})))
+        for(auto blockIdx : onAcc::makeIter(acc, onAcc::worker::blocksInGrid, IdxRange{numBlocks}))
         {
             auto const numDataElemInBlock = acc[frame::extent];
             auto blockOffset = blockIdx * numDataElemInBlock;
-            for(auto inBlockOffset : onAcc::makeIter(acc, onAcc::iter::withinDataFrame))
+            for(auto inBlockOffset : onAcc::makeIter(acc, onAcc::worker::threadsInBlock, onAcc::range::frameExtent))
             {
                 out[blockOffset + inBlockOffset] = (blockOffset + inBlockOffset).x();
             }
