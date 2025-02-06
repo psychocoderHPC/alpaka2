psychocoderHPC/alpaka2

commit d29b37b91556c3535a12e895c4e11f6ca84275fe

support compiletime number of threads per block
- take care that a compile time definition of frame extents reults into
  compile-time number of threads per block
- ensure that compile time number of threads is propergated as compile
  time value into the kernel
- add test to check that number of threads per block is compile time if
  required

include/alpaka/api/cuda/IdxLayer.hpp
@@ -14,31 +14,40 @@ namespace alpaka::onAcc
 {
     namespace unifiedCudaHip
     {
-        template<typename T_IdxType, uint32_t T_dim>
+        template<typename T_NumBlocksType>
         struct BlockLayer
         {
             constexpr auto idx() const
             {
-                return Vec<T_IdxType, 3u>{::blockIdx.z, ::blockIdx.y, ::blockIdx.x}.template rshrink<T_dim>();
+                return Vec<typename T_NumBlocksType::type, 3u>{::blockIdx.z, ::blockIdx.y, ::blockIdx.x}
+                    .template rshrink<T_NumBlocksType::dim()>();
             }
 
             constexpr auto count() const
             {
-                return Vec<T_IdxType, 3u>{::gridDim.z, ::gridDim.y, ::gridDim.x}.template rshrink<T_dim>();
+                return Vec<typename T_NumBlocksType::type, 3u>{::gridDim.z, ::gridDim.y, ::gridDim.x}
+                    .template rshrink<T_NumBlocksType::dim()>();
             }
         };
 
-        template<typename T_IdxType, uint32_t T_dim>
+        template<typename T_NumThreadsType>
         struct ThreadLayer
         {
             constexpr auto idx() const
             {
-                return Vec<T_IdxType, 3u>{::threadIdx.z, ::threadIdx.y, ::threadIdx.x}.template rshrink<T_dim>();
+                return Vec<typename T_NumThreadsType::type, 3u>{::threadIdx.z, ::threadIdx.y, ::threadIdx.x}
+                    .template rshrink<T_NumThreadsType::dim()>();
             }
 
             constexpr auto count() const
             {
-                return Vec<T_IdxType, 3u>{::blockDim.z, ::blockDim.y, ::blockDim.x}.template rshrink<T_dim>();
+                return Vec<typename T_NumThreadsType::type, 3u>{::threadIdx.z, ::threadIdx.y, ::threadIdx.x}
+                    .template rshrink<T_NumThreadsType::dim()>();
+            }
+
+            constexpr auto count() const requires alpaka::concepts::CVector<T_NumThreadsType>
+            {
+                return T_NumThreadsType{};
             }
         };
     } // namespace unifiedCudaHip

include/alpaka/api/cpu/IdxLayer.hpp
@@ -24,9 +24,19 @@ namespace alpaka::onAcc
                 return IndexVecType::all(0);
             }
 
+            constexpr auto idx() const requires alpaka::concepts::CVector<IndexVecType>
+            {
+                return IndexVecType::template all<0>();
+            }
+
             constexpr auto count() const
             {
                 return IndexVecType::all(1);
+            }
+
+            constexpr auto count() const requires alpaka::concepts::CVector<IndexVecType>
+            {
+                return IndexVecType::template all<1u>();
             }
         };
 

include/alpaka/api/hip/IdxLayer.hpp
@@ -14,31 +14,40 @@ namespace alpaka::onAcc
 {
     namespace unifiedCudaHip
     {
-        template<typename T_IdxType, uint32_t T_dim>
+        template<typename T_NumBlocksType>
         struct BlockLayer
         {
             constexpr auto idx() const
             {
-                return Vec<T_IdxType, 3u>{hipBlockIdx_z, hipBlockIdx_y, hipBlockIdx_x}.template rshrink<T_dim>();
+                return Vec<typename T_NumBlocksType::type, 3u>{hipBlockIdx_z, hipBlockIdx_y, hipBlockIdx_x}
+                    .template rshrink<T_NumBlocksType::dim()>();
             }
 
             constexpr auto count() const
             {
-                return Vec<T_IdxType, 3u>{hipGridDim_z, hipGridDim_y, hipGridDim_x}.template rshrink<T_dim>();
+                return Vec<typename T_NumBlocksType::type, 3u>{hipGridDim_z, hipGridDim_y, hipGridDim_x}
+                    .template rshrink<T_NumBlocksType::dim()>();
             }
         };
 
-        template<typename T_IdxType, uint32_t T_dim>
+        template<typename T_NumThreadsType>
         struct ThreadLayer
         {
             constexpr auto idx() const
             {
-                return Vec<T_IdxType, 3u>{hipThreadIdx_z, hipThreadIdx_y, hipThreadIdx_x}.template rshrink<T_dim>();
+                return Vec<typename T_NumThreadsType::type, 3u>{hipThreadIdx_z, hipThreadIdx_y, hipThreadIdx_x}
+                    .template rshrink<T_NumThreadsType::dim()>();
             }
 
             constexpr auto count() const
             {
-                return Vec<T_IdxType, 3u>{hipBlockDim_z, hipBlockDim_y, hipBlockDim_x}.template rshrink<T_dim>();
+                return Vec<typename T_NumThreadsType::type, 3u>{hipBlockDim_z, hipBlockDim_y, hipBlockDim_x}
+                    .template rshrink<T_NumThreadsType::dim()>();
+            }
+
+            constexpr auto count() const requires alpaka::concepts::CVector<T_NumThreadsType>
+            {
+                return T_NumThreadsType{};
             }
         };
     } // namespace unifiedCudaHip

include/alpaka/Vec.hpp
@@ -79,6 +79,22 @@ namespace alpaka
                     Values{});
                 return result;
             }
+
+            template<T T_value>
+            static constexpr auto all()
+            {
+                using IotaSeq = std::make_integer_sequence<T, dim()>;
+                return integerSequenceToCVec(IotaSeq{}, [](auto&&) constexpr { return T_value; });
+            }
+
+        private:
+            template<T... T_indecies>
+            static constexpr auto integerSequenceToCVec(
+                std::integer_sequence<T, T_indecies...>,
+                auto const op = std::identity{})
+            {
+                return CVec<T, op(T_indecies)...>{};
+            };
         };
 
         template<typename T>
@@ -212,6 +228,12 @@ namespace alpaka
                 Vec result([=](uint32_t const) { return value; });
                 return result;
             }
+        }
+
+        template<T_Type T_v>
+        static constexpr auto all() requires requires { T_Storage::template all<T_v>(); }
+        {
+            return Vec<T_Type, T_dim, ALPAKA_TYPEOF(T_Storage::template all<T_v>())>{};
         }
 
         constexpr Vec toRT() const

include/alpaka/api/cpu/Device.hpp
@@ -176,6 +176,16 @@ namespace alpaka::onHost
                 cpu::Device<T_Platform> const& device,
                 T_Mapping const& executor,
                 FrameSpec<T_NumBlocks, T_NumThreads> const& dataBlocking,
+                T_KernelBundle const& kernelBundle) const requires alpaka::concepts::CVector<T_NumThreads>
+            {
+                auto numThreadBlocks = dataBlocking.getThreadSpec().m_numBlocks;
+                return ThreadSpec{numThreadBlocks, T_NumThreads::template all<1u>()};
+            }
+
+            auto operator()(
+                cpu::Device<T_Platform> const& device,
+                T_Mapping const& executor,
+                FrameSpec<T_NumBlocks, T_NumThreads> const& dataBlocking,
                 T_KernelBundle const& kernelBundle) const
             {
                 auto numThreadBlocks = dataBlocking.getThreadSpec().m_numBlocks;
@@ -212,6 +222,17 @@ namespace alpaka::onHost
             FrameSpec<T_NumBlocks, T_NumThreads>,
             T_KernelBundle>
         {
+            auto operator()(
+                cpu::Device<T_Platform> const& device,
+                exec::CpuOmpBlocksAndThreads const& executor,
+                FrameSpec<T_NumBlocks, T_NumThreads> const& dataBlocking,
+                T_KernelBundle const& kernelBundle) const requires alpaka::concepts::CVector<T_NumThreads>
+            {
+                auto numThreadBlocks = dataBlocking.getThreadSpec().m_numBlocks;
+
+                return ThreadSpec{numThreadBlocks, T_NumThreads::template all<1u>()};
+            }
+
             auto operator()(
                 cpu::Device<T_Platform> const& device,
                 exec::CpuOmpBlocksAndThreads const& executor,

include/alpaka/api/cpu/exec/OmpThreads.hpp
@@ -84,7 +84,7 @@ namespace alpaka::onHost
                         auto const blockSharedMemEntry = DictEntry{layer::shared, std::ref(blockSharedMem)};
                         auto const blockSyncEntry = DictEntry{action::sync, onAcc::cpu::OmpSync{}};
 
-                        using NumThreadsVecType = typename ThreadSpecType::NumThreadsVecType;
+                        using NumThreadsVecType = typename ThreadSpecType::NumThreadsVecType::UniVec;
                         using ThreadIdxType = typename NumThreadsVecType::type;
 #    pragma omp for
                         for(ThreadIdxType i = 0; i < blockCountND.product(); ++i)

include/alpaka/api/unifiedCudaHip/Device.hpp
@@ -204,6 +204,15 @@ namespace alpaka::onHost
                 unifiedCudaHip::Device<T_Platform> const& device,
                 T_Mapping const& executor,
                 FrameSpec<T_NumBlocks, T_NumThreads> const& dataBlocking,
+                T_KernelBundle const& kernelBundle) const requires alpaka::concepts::CVector<T_NumThreads>
+            {
+                return dataBlocking.getThreadSpec();
+            }
+
+            auto operator()(
+                unifiedCudaHip::Device<T_Platform> const& device,
+                T_Mapping const& executor,
+                FrameSpec<T_NumBlocks, T_NumThreads> const& dataBlocking,
                 T_KernelBundle const& kernelBundle) const
             {
                 auto numThreadBlocks = dataBlocking.getThreadSpec().m_numBlocks;

include/alpaka/api/unifiedCudaHip/Queue.hpp
@@ -108,8 +108,8 @@ namespace alpaka::onHost
         template<
             typename T_Api,
             typename T_Executor,
-            typename T_IdxType,
-            uint32_t T_dim,
+            typename T_NumBlocksType,
+            typename T_NumThreadsType,
             typename TKernelBundle,
             typename T_NumFrames,
             typename T_FrameSize>
@@ -120,8 +120,8 @@ namespace alpaka::onHost
         {
             auto acc = onAcc::Acc{
                 Dict{
-                    DictEntry(layer::block, onAcc::unifiedCudaHip::BlockLayer<T_IdxType, T_dim>{}),
-                    DictEntry(layer::thread, onAcc::unifiedCudaHip::ThreadLayer<T_IdxType, T_dim>{}),
+                    DictEntry(layer::block, onAcc::unifiedCudaHip::BlockLayer<T_NumBlocksType>{}),
+                    DictEntry(layer::thread, onAcc::unifiedCudaHip::ThreadLayer<T_NumThreadsType>{}),
                     DictEntry(frame::count, numFrames),
                     DictEntry(frame::extent, frameExtent),
                     DictEntry(action::sync, onAcc::unifiedCudaHip::Sync{}),
@@ -131,13 +131,18 @@ namespace alpaka::onHost
             kernelBundle(acc);
         }
 
-        template<typename T_Api, typename T_Executor, typename T_IdxType, uint32_t T_dim, typename TKernelBundle>
+        template<
+            typename T_Api,
+            typename T_Executor,
+            typename T_NumBlocksType,
+            typename T_NumThreadsType,
+            typename TKernelBundle>
         __global__ void gpuKernel(TKernelBundle const kernelBundle)
         {
             auto acc = onAcc::Acc{
                 Dict{
-                    DictEntry(layer::block, onAcc::unifiedCudaHip::BlockLayer<T_IdxType, T_dim>{}),
-                    DictEntry(layer::thread, onAcc::unifiedCudaHip::ThreadLayer<T_IdxType, T_dim>{}),
+                    DictEntry(layer::block, onAcc::unifiedCudaHip::BlockLayer<T_NumBlocksType>{}),
+                    DictEntry(layer::thread, onAcc::unifiedCudaHip::ThreadLayer<T_NumThreadsType>{}),
                     DictEntry(action::sync, onAcc::unifiedCudaHip::Sync{}),
                     DictEntry(object::api, T_Api{}),
                     DictEntry(object::exec, T_Executor{})},
@@ -145,16 +150,16 @@ namespace alpaka::onHost
             kernelBundle(acc);
         }
 
-        template<typename TIdx, uint32_t T_dim>
-        ALPAKA_FN_HOST auto convertVecToUniformCudaHipDim(Vec<TIdx, T_dim> const& vec) -> dim3
+        ALPAKA_FN_HOST auto convertVecToUniformCudaHipDim(alpaka::concepts::Vector auto const& vec) -> dim3
         {
+            constexpr auto vecDim = ALPAKA_TYPEOF(vec)::dim();
             dim3 dim(1, 1, 1);
-            if constexpr(T_dim >= 1u)
-                dim.x = static_cast<unsigned>(vec[T_dim - 1u]);
-            if constexpr(T_dim >= 2u)
-                dim.y = static_cast<unsigned>(vec[T_dim - 2u]);
-            if constexpr(T_dim >= 3u)
-                dim.z = static_cast<unsigned>(vec[T_dim - 3u]);
+            if constexpr(vecDim >= 1u)
+                dim.x = static_cast<unsigned>(vec[vecDim - 1u]);
+            if constexpr(vecDim >= 2u)
+                dim.y = static_cast<unsigned>(vec[vecDim - 2u]);
+            if constexpr(vecDim >= 3u)
+                dim.z = static_cast<unsigned>(vec[vecDim - 3u]);
 
             return dim;
         }
@@ -183,8 +188,8 @@ namespace alpaka::onHost
                 auto kernelName = gpuKernel<
                     ALPAKA_TYPEOF(onHost::getApi(queue)),
                     T_Executor,
-                    typename T_NumBlocks::type,
-                    T_NumBlocks::dim(),
+                    T_NumBlocks,
+                    T_NumThreads,
                     T_KernelBundle,
                     T_Args...>;
 

commit 6e6aa72357452dda11db7e90f82b43eb7f2d4640

fix CUDA IndexLayer
fix bug introduced with #42

include/alpaka/api/cuda/IdxLayer.hpp
@@ -41,7 +41,7 @@ namespace alpaka::onAcc
 
             constexpr auto count() const
             {
-                return Vec<typename T_NumThreadsType::type, 3u>{::threadIdx.z, ::threadIdx.y, ::threadIdx.x}
+                return Vec<typename T_NumThreadsType::type, 3u>{::blockDim.z, ::blockDim.y, ::blockDim.x}
                     .template rshrink<T_NumThreadsType::dim()>();
             }
 