10f4c782bea68e1c3fc52933ef2feedc3b2df923
René Widera <r.widera@hzdr.de>
2026-07-08 11:39:09 +0200

    CI: report more device details
    In case a job is failing this PR is reporting a detailed overview about
    the device.

--- test/include/alpakaTest/deviceHelper.hpp
+++ test/include/alpakaTest/deviceHelper.hpp
@@ -50,7 +50,8 @@ namespace alpaka::test
         }
 
         onHost::Device device = devSelector.makeDevice(0);
-        UNSCOPED_INFO("Device: " << device.getName());
+        UNSCOPED_INFO("Device: \n" << device.getDeviceProperties());
+        UNSCOPED_INFO("Free memory (bytes): " << device.getFreeGlobalMemBytes());
         return device;
     }
 
