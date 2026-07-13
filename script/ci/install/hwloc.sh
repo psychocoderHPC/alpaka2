370d24ad7811ffb51ef69addca5c590bedbdf43f
Simeon Ehrig <s.ehrig@hzdr.de>
2026-07-13 17:07:35 +0200

    CI: install hwloc via script

---
+++ script/ci/install/hwloc.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2026 Simeon Ehrig
+# SPDX-License-Identifier: MPL-2.0
+#
+
+: "${APCI_ALPAKA_ROOT?'APCI_ALPAKA_ROOT is not defined. Root directory of the alpaka project'}"
+# shellcheck source=script/ci/utils/default.sh
+source "${APCI_ALPAKA_ROOT}/script/ci/utils/default.sh"
+
+if [[ "$APCI_OS_NAME" != "Linux" ]]; then
+    exit_error "Install hwloc script does not support Windows or MacOS"
+fi
+
+script_msg "Install APCI_HWLOC"
+
+if [[ "${APCI_HWLOC}" == "ON" ]]; then
+    install_msg "hwloc"
+
+    hwloc_package_list=(libhwloc-dev pkg-config)
+
+    if dpkg -s "${hwloc_package_list[@]}" >/dev/null 2>&1; then
+        echo_yellow "hwloc is already installed via apt. Skip installation."
+    else
+        lazy_apt_update
+        quiet_run sudo DEBIAN_FRONTEND=noninteractive apt install -y "${hwloc_package_list[@]}"
+    fi
+    echo_run which pkg-config
+    echo "hwloc version: $(pkg-config --modversion hwloc)"
+    echo_run dpkg -L libhwloc-dev
+else
+    echo_green "Skipped install hwloc because it is not required for the job."
+fi
