16f29bb649e9e845d517f6c7280b0a5f37c82333
TimHanel00 <tim.hanel00@gmail.com>
2026-07-29 11:01:47 +0200

    add CMake clang-tidy test

---
+++ clangTidy/src/main.cpp
@@ -0,0 +1,10 @@
+/* Copyright 2026 Tim Hanel
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("clangTidyTestMain", "[clangTidy]")
+{
+    REQUIRE(true);
+}
